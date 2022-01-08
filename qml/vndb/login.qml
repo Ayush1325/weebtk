45dfa9aee0807848182ad3b4a6ab6b3dd6201acc
Ayush Singh <ayushsingh1325@gmail.com>
2022-01-08 16:13:34 +0530

    Added Login Page for vndb.

---
+++ qml/vndb/login.qml
@@ -0,0 +1,27 @@
+import QtQuick 2.6
+import QtQuick.Layouts 1.2
+import QtQuick.Controls 2.2 as Controls
+import org.kde.kirigami 2.13 as Kirigami
+
+Kirigami.ScrollablePage {
+    title: i18nc("@title", "VNDB Login")
+
+    Kirigami.FormLayout {
+        id: layout
+        Layout.fillWidth: true
+
+        Controls.TextField {
+            Kirigami.FormData.label: "Username:"
+        }
+        Controls.TextField {
+            Kirigami.FormData.label: "Password:"
+        }
+        Controls.Button {
+            Layout.fillWidth: true
+            text: i18n("Submit")
+            onClicked: {
+                console.log("Submit")
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,16 @@
-use cstr::cstr;
 use ki18n::klocalizedcontext::KLocalizedContext;
 use qmetaobject::prelude::*;
 use qmetaobject::QUrl;
 
-// The `QObject` custom derive macro allows to expose a class to Qt and QML
-#[derive(QObject, Default)]
-struct Greeter {
-    // Specify the base class with the qt_base_class macro
-    base: qt_base_class!(trait QObject),
-    // Declare `name` as a property usable from Qt
-    name: qt_property!(QString; NOTIFY name_changed),
-    // Declare a signal
-    name_changed: qt_signal!(),
-    // And even a slot
-    compute_greetings: qt_method!(
-        fn compute_greetings(&self, verb: String) -> QString {
-            format!("{} {}", verb, self.name.to_string()).into()
-        }
-    ),
-}
-
 qrc!(root_qml,
-    "" {
-        "qml/main.qml" as "main.qml",
+    "qml" as "" {
+        "main.qml",
+    }
+);
+
+qrc!(vndb_qml,
+    "qml/vndb" as "vndb" {
+        "login.qml",
     }
 );
 
@@ -30,12 +18,13 @@ fn main() {
     qmetaobject::log::init_qt_to_rust();
     env_logger::init();
 
-    qml_register_type::<Greeter>(cstr!("Greeter"), 1, 0, cstr!("Greeter"));
     let mut engine = QmlEngine::new();
 
     KLocalizedContext::init_from_engine(&engine);
 
     root_qml();
+    vndb_qml();
+
     engine.load_url(QUrl::from(QString::from("qrc:///main.qml")));
     engine.exec();
 }
